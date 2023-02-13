7f358913bc3b953dca307a3828eb69e3097ad2bb
muthu ram <mr032495@gmail.com>
2023-02-13 22:22:58 +0530

    first

---
+++ Pattern/C/start.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a=1,i=1;
+    while(a<=5){
+        while(i<=5){
+            printf("*");
+            i++;
+        }
+        a++;
+    }
+}
